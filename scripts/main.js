0b0aa2d0740fe436b6370435095433830fd95dc5
toddt <toddteis@gmail.com>
2022-07-12 14:23:05 +1000

    Added foodRepo[] array containing food item objects. Added logic to orderingSystem.placeOrder() that returns receipt output.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,26 @@
+const foodRepo = [
+  {
+    foodName: 'Burger',
+    restuarant: 'Wild Burger Joint',
+    price: 15,
+  },
+  {
+    foodName: 'Pepperoni Pizza',
+    restuarant: 'Awesome Pizza Place',
+    price: 20,
+  },
+];
+
 // eslint-disable-next-line no-unused-vars
 const orderingSystem = (() => {
   const placeOrder = (food) => {
-    console.log(`You ordered: ${food}`);
+    if (food) {
+      const foodLowerCase = food.toLowerCase();
+      const filteredItems = foodRepo.filter((item) => item.foodName.toLowerCase() === foodLowerCase);
+      const buildOutput = `${filteredItems[0].restuarant}, ${filteredItems[0].foodName}, $${filteredItems[0].price}`;
+      return buildOutput;
+    }
+    return 'Please enter a food';
   };
 
   return {
