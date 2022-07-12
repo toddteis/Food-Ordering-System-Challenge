789665f690a5fae81b0291f7a1255cddbba7f93e
toddt <toddteis@gmail.com>
2022-07-12 12:29:29 +1000

    Added a module called orderingSystem and created a method called placeOrder().

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+const orderingSystem = (() => {
+  const placeOrder = (food) => {
+    console.log(`You ordered: ${food}`);
+  };
+
+  return {
+    placeOrder,
+  };
+})();
