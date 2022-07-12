9b0c01630328ec995f48350c802fa0a8eb5706a5
toddt <toddteis@gmail.com>
2022-07-13 06:49:15 +1000

    Added logic to test and handle an empty parameter when calling orderingSystem.placeOrder().

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 const foodRepo = [
   {
-    foodName: 'Burger',
+    food: 'Burger',
     restuarant: 'Wild Burger Joint',
     price: 15,
   },
   {
-    foodName: 'Pepperoni Pizza',
+    food: 'Pepperoni Pizza',
     restuarant: 'Awesome Pizza Place',
     price: 20,
   },
@@ -15,12 +15,17 @@ const foodRepo = [
 const orderingSystem = (() => {
   const placeOrder = (food) => {
     if (food) {
-      const foodLowerCase = food.toLowerCase();
-      const filteredItems = foodRepo.filter((item) => item.foodName.toLowerCase() === foodLowerCase);
-      const buildOutput = `${filteredItems[0].restuarant}, ${filteredItems[0].foodName}, $${filteredItems[0].price}`;
+      const filtered = foodRepo.filter((item) => item.food.toLowerCase() === food.toLowerCase());
+      const buildOutput = `${filtered[0].restuarant}, ${filtered[0].food}, $${filtered[0].price}`;
       return buildOutput;
     }
-    return 'Please enter a food';
+
+    const menu = [];
+    for (let i = 0; i < foodRepo.length; i += 1) {
+      menu.push(foodRepo[i].food);
+    }
+
+    return `Please enter a food from our menu: ${menu}`;
   };
 
   return {
