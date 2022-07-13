924514461e7761f91120180469bed5bfe014f7a7
toddt <toddteis@gmail.com>
2022-07-14 07:17:34 +1000

    Moved menu data into new Menu Module. Renamed some variables.

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,47 @@
-const foodRepo = [
-  {
-    food: 'Burger',
-    restuarant: 'Wild Burger Joint',
-    price: 15,
-  },
-  {
-    food: 'Pepperoni Pizza',
-    restuarant: 'Awesome Pizza Place',
-    price: 20,
-  },
-  {
-    food: 'Chips',
-    restuarant: 'Best Chippery',
-    price: 10,
-  },
-];
+const menu = (() => {
+  const repo = [
+    {
+      food: 'Burger',
+      restuarant: 'Wild Burger Joint',
+      price: 15,
+    },
+    {
+      food: 'Pepperoni Pizza',
+      restuarant: 'Awesome Pizza Place',
+      price: 20,
+    },
+    {
+      food: 'Chips',
+      restuarant: 'Best Chippery',
+      price: 10,
+    },
+  ];
+
+  function getMenu() {
+    return repo;
+  }
+
+  return {
+    getMenu,
+  };
+})();
 
 // eslint-disable-next-line no-unused-vars
 const orderingSystem = (() => {
   const placeOrder = (food) => {
     if (food) {
-      const filtered = foodRepo.filter((item) => item.food.toLowerCase() === food.toLowerCase());
+      const foodLC = food.toLowerCase();
+      const filtered = menu.getMenu().filter((item) => item.food.toLowerCase() === foodLC);
       const result = `${filtered[0].restuarant}, ${filtered[0].food}, $${filtered[0].price}`;
       return result;
     }
 
-    const menu = [];
-    for (let i = 0; i < foodRepo.length; i += 1) {
-      menu.push(foodRepo[i].food);
+    const foodNames = [];
+    for (let i = 0; i < menu.getMenu().length; i += 1) {
+      foodNames.push(menu.getMenu()[i].food);
     }
 
-    const result = `Please enter a food from our menu: ${menu.join(', ')}`;
+    const result = `Please enter a food from our menu: ${foodNames.join(', ')}`;
 
     return result;
   };
