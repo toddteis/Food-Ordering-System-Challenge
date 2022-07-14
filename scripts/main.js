caa88ca9969a63f7cfeb0c4ad7232d1c4f6bf6c2
toddt <toddteis@gmail.com>
2022-07-15 06:56:50 +1000

    Added Factory Module and moved object creation to factory.menuItem. Added menu.getFoodNames() for displaying the menu for empty or incorrect orders.

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,40 @@
-const menu = (() => {
-  const repo = [
-    {
-      food: 'Burger',
-      restuarant: 'Wild Burger Joint',
-      price: 15,
-    },
-    {
-      food: 'Pepperoni Pizza',
-      restuarant: 'Awesome Pizza Place',
-      price: 20,
-    },
-    {
-      food: 'Chips',
-      restuarant: 'Best Chippery',
-      price: 10,
-    },
-  ];
+const factory = (() => {
+  const menuItem = (food, restuarant, price) => {
+    const getFoodName = () => food;
+    const getRestuarant = () => restuarant;
+    const getPrice = () => price;
+    return { getFoodName, getRestuarant, getPrice };
+  };
+  return { menuItem };
+})();
 
-  function getMenu() {
+const menu = (() => {
+  const repo = [];
+  // Create menu items and add to repo
+  const menuItemBurger = factory.menuItem('Burger', 'Wild Burger Join', '15');
+  repo.push(menuItemBurger);
+  const menuItemPizza = factory.menuItem('Pepperoni Pizza', 'Awesome Pizza Place', '20');
+  repo.push(menuItemPizza);
+  const menuItemChips = factory.menuItem('Chips', 'Best Ever Chippery', '10');
+  repo.push(menuItemChips);
+
+  function getDetailedMenu() {
     return repo;
   }
 
+  function getFoodNames() {
+    const foodNames = [];
+    for (let i = 0; i < menu.getDetailedMenu().length; i += 1) {
+      foodNames.push(menu.getDetailedMenu()[i].getFoodName());
+    }
+    const result = foodNames.join(', ');
+
+    return result;
+  }
+
   return {
-    getMenu,
+    getDetailedMenu,
+    getFoodNames,
   };
 })();
 
@@ -31,17 +43,16 @@ const orderingSystem = (() => {
   const placeOrder = (food) => {
     if (food) {
       const foodLC = food.toLowerCase();
-      const filtered = menu.getMenu().filter((item) => item.food.toLowerCase() === foodLC);
-      const result = `${filtered[0].restuarant}, ${filtered[0].food}, $${filtered[0].price}`;
+      const detailedMenu = menu.getDetailedMenu();
+      const filtered = detailedMenu.filter((item) => item.getFoodName().toLowerCase() === foodLC);
+      if (filtered.length === 0) {
+        return `Sorry, "${food}" isn't on our menu, please select from: ${menu.getFoodNames()}`;
+      }
+      const result = `${filtered[0].getRestuarant()}, ${filtered[0].getFoodName()}, $${filtered[0].getPrice()}`;
       return result;
     }
 
-    const foodNames = [];
-    for (let i = 0; i < menu.getMenu().length; i += 1) {
-      foodNames.push(menu.getMenu()[i].food);
-    }
-
-    const result = `Please enter a food from our menu: ${foodNames.join(', ')}`;
+    const result = `Please enter a food from our menu: ${menu.getFoodNames()}`;
 
     return result;
   };
